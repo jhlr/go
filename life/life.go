0cab7030a9664997390a89e34eb50a0634758557
Joao Rietra <jhlrietra@gmail.com>
2017-01-27 13:11:42 -0300

    Update life.go

--- life/life.go
+++ life/life.go
@@ -1,37 +1,53 @@
 package life
 
+import "image"
+
+// cellsize has to be >=2
+const cellsize = 2
+
 type Universe struct {
-	r     Rule
+	rule  Rule
 	count uint
-	board map[Position][2]bool
+	board map[image.Point][cellsize + 1]bool
 }
 
 func New(r Rule) *Universe {
 	u := new(Universe)
-	u.r = r
+	u.rule = r
 	u.count = 0
-	u.board = make(map[Position][2]bool)
+	u.board = make(map[image.Point][cellsize + 1]bool)
 	return u
 }
 
 func (u *Universe) Next() {
-	c := u.count % 2
+	s := u.count % cellsize
 	for p := range u.board {
-		u.Around(p, func(q Position) {
-			b := u.next(q, c)
-			u.set(q, 1-c, b)
+		u.Around(p, func(q image.Point) {
+			if !u.get(q, cellsize) {
+				b := u.foresee(q, s)
+				u.set(q, 1-s, b)
+			}
 		})
 	}
 	u.count++
-	u.trim()
+	u.Update()
 }
 
-func (u *Universe) Set(p Position, b bool) {
-	u.set(p, u.count%2, b)
+func (u *Universe) Around(p image.Point, foo func(image.Point)) {
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			q := Pt(p.X+i, p.Y+j)
+			foo(q)
+		}
+	}
 }
 
-func (u *Universe) Get(p Position) bool {
-	return u.get(p, u.count%2)
+func (u *Universe) Set(p image.Point, b bool) {
+	u.set(p, u.count%cellsize, b)
+}
+
+func (u *Universe) Get(p image.Point) bool {
+	return u.get(p, u.count%cellsize)
 }
 
 func (u *Universe) Count() uint {
@@ -39,47 +55,41 @@ func (u *Universe) Count() uint {
 }
 
 func (u *Universe) Rule() Rule {
-	return u.r
+	return u.rule
 }
 
-func (u *Universe) Around(p Position, foo func(Position)) {
-	for i := -1; i <= 1; i++ {
-		for j := -1; j <= 1; j++ {
-			q := P(p.X+i, p.Y+j)
-			foo(q)
-		}
-	}
-}
-
-func (u *Universe) trim() {
-	c := u.count % 2
+func (u *Universe) Update() {
+	s := u.count % cellsize
 	for p := range u.board {
-		if !u.get(p, c) {
+		if !u.get(p, s) {
 			delete(u.board, p)
+		} else {
+			u.set(p, cellsize, false)
 		}
 	}
 }
 
-func (u *Universe) get(p Position, c uint) bool {
-	return u.board[p][c]
+func (u *Universe) get(p image.Point, s uint) bool {
+	return u.board[p][s]
 }
 
-func (u *Universe) set(p Position, c uint, b bool) {
+func (u *Universe) set(p image.Point, s uint, b bool) {
 	cell := u.board[p]
-	cell[c] = b
+	cell[cellsize] = true
+	cell[s] = b
 	u.board[p] = cell
 }
 
-func (u *Universe) next(p Position, c uint) bool {
+func (u *Universe) foresee(p image.Point, s uint) bool {
 	alive := 0
-	u.Around(p, func(q Position) {
-		if u.get(q, c) {
+	u.Around(p, func(q image.Point) {
+		if u.get(q, s) {
 			alive++
 		}
 	})
-	if u.get(p, c) {
+	if u.get(p, s) {
 		alive--
-		return u.r[1][alive]
+		return u.rule[1][alive]
 	}
-	return u.r[0][alive]
+	return u.rule[0][alive]
 }
