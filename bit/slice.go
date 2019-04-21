c9269793f40c5be89e9356832c08f89ee063868c
Joao Rietra <jhlrietra@gmail.com>
2019-04-21 18:49:01 -0300

    Major changes

--- bit/slice.go
+++ bit/slice.go
@@ -1,52 +1,97 @@
 package bit
 
 // Slice is a slice of bits
-type Slice []uint8
-
-// New makes a bit slice of given bit capacity
-func New(len int) Slice {
-	if len < 0 {
-		_ = make([]uint8, len)
-	}
-	s := make([]uint8, len/8+1)
-	return Slice(s)
+type Slice struct {
+	data []uint8
+	len  uint
 }
 
-// Len is the bit capacity
-func (s Slice) Len() int {
-	return len(s) * 8
+// BoundError is panicked when
+// the accessed value is bigger than len
+type BoundError struct{}
+
+func (e BoundError) Error() string {
+	return "out of bounds"
+}
+
+// Make makes a bit slice of given bit capacity
+func Make(len uint, cap uint) *Slice {
+	if len > cap {
+		panic(BoundError{})
+	}
+	s := new(Slice)
+	s.len = len
+	cap += cap % 8
+	s.data = make([]uint8, cap/8)
+	return s
+}
+
+// Len is the bit length
+func (s *Slice) Len() uint {
+	return s.len
+}
+
+// Cap is the bit capacity
+func (s *Slice) Cap() uint {
+	return uint(len(s.data)) * 8
 }
 
 // Fill sets all bits as the given value
-func (s Slice) Fill(val bool) {
+func (s *Slice) Fill(val bool) {
 	mask := uint8(0)
 	if val {
 		mask = ^mask
 	}
-	for i := range s {
-		s[i] = mask
+	for i := s.len - 1; i >= 0; i-- {
+		s.data[i] = mask
 	}
 }
 
-// Set sets individual bits
-func (s Slice) Set(i uint, val bool) {
+// Set individual bits
+func (s *Slice) Set(i uint, val bool) {
+	if i >= s.len {
+		panic(BoundError{})
+	}
 	mask := uint8(1 << (i % 8))
 	if val {
-		s[i/8] |= mask
+		s.data[i/8] |= mask
 	} else {
-		s[i/8] &^= mask
+		s.data[i/8] &^= mask
 	}
 }
 
-// Get gets individual bits
-func (s Slice) Get(i uint) bool {
+// Get individual bits
+func (s *Slice) Get(i uint) bool {
+	if i >= s.len {
+		panic(BoundError{})
+	}
 	mask := uint8(1 << (i % 8))
-	return s[i/8]&mask != 0
+	return s.data[i/8]&mask != 0
 }
 
-// Copy will allocate an exact copy
-func (s Slice) Copy() Slice {
-	result := make([]uint8, len(s))
-	copy(result, s)
-	return result
+// Append works like the builtin append
+func (s *Slice) Append(vals ...bool) {
+	n := uint(len(vals))
+	if s.len > s.Cap() {
+		len := s.len + n
+		len = (len + len%8) / 8
+		temp := make([]uint8, len)
+		copy(temp, s.data)
+		s.data = temp
+	}
+	for i, b := range vals {
+		if b {
+			s.Set(s.len+uint(i), b)
+		}
+	}
+	s.len += n
+}
+
+// SetLength will panic if argument is more than s.Len()
+func (s *Slice) SetLength(len uint) {
+	if len <= s.Len() {
+		s.len = len
+	} else {
+		panic(BoundError{})
+	}
 }
